b198501336951848365291a30f1497250be9acdd
ACT3 <chuiC456@163.com>
2020-11-03 22:49:13 +0800

    Add tbg-find-replace.el & README.org files.

---
+++ tbg-find-replace.el
@@ -0,0 +1,21 @@
+;;; tbg-find-replace.el --- ▮ -*- coding: utf-8; lexical-binding: t; -*-
+
+;; Copyright © 2020-2027, by 3badguys
+
+;; Author: 3badguys
+;; Version: ▮
+;; Created: ▮
+;; Package-Requires: ▮
+;; Keywords: ▮
+;; License: GPL v3
+
+;; This file is not part of GNU Emacs.
+
+;;; Commentary:
+
+;;; Code:
+
+
+
+
+;;; tbg-find-replace.el ends here
